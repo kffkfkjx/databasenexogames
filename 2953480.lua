758bd9d584cb8ccdc9d8ff894f37fd6066494f31
iSeif19 <seifelsayed1669@gmail.com>
2025-06-11 05:13:51 +0300

    Add files via upload

---
+++ 2953480.lua
@@ -0,0 +1,20 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 2953480 Manifest
+-- Name: Lunar Eclipse
+-- Generated: 2025-06-11 03:39:51
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2953480) -- Lunar Eclipse
+
+-- MAIN APP DEPOTS
+addappid(2953481, 1, "2a966395acc7d2cc1c60362d668bae6e0b283d86ba2251ae54a6ca3151d39c09") -- Main Game Content (Windows Content)
+setManifestid(2953481, "6469505038192337857", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
