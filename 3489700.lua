c6da69842bef437fbdc1aca328659dbad0293132
iSeif19 <seifelsayed1669@gmail.com>
2025-06-12 03:06:25 +0300

    Add files via upload

---
+++ 3489700.lua
@@ -0,0 +1,7 @@
+-- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
+-- Redistribution of VICTORs files is not allowed AT ALL
+-- Join the official here: https://discord.gg/piracylords
+
+addappid(3489700)
+addappid(3489701,1,"2266ca31f6f2666ebe75906201e72f9398694030ff1c8214aacebcf74dc94ee3")
+setManifestid(3489701, "1840880717473894999")
